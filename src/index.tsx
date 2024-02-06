a0f364f085c26feefd13f79ec7d73c090589c788
GeorgiyIzmailov <georgiy-izmailov-v@inkeep.com>
2024-02-06 17:03:18 +0300

    create integration

--- src/index.tsx
+++ src/index.tsx
@@ -3,54 +3,149 @@ import {
     createComponent,
     FetchEventCallback,
     RuntimeContext,
-  } from "@gitbook/runtime";
+} from '@gitbook/runtime';
 
-  type IntegrationContext = {} & RuntimeContext;
-  type IntegrationBlockProps = {};
-  type IntegrationBlockState = { message: string };
-  type IntegrationAction = { action: "click" };
+type IntegrationContext = {} & RuntimeContext;
+type IntegrationBlockProps = { content?: string };
+type IntegrationBlockState = { content: string };
+type IntegrationAction = {};
 
-  const handleFetchEvent: FetchEventCallback<IntegrationContext> = async (
-    request,
-    context
-  ) => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { api } = context;
-    const user = api.user.getAuthenticatedUser();
+const WIDGET_VERSION = '0.2.258';
 
-    return new Response(JSON.stringify(user));
-  };
+const addInkeepWidget = () => {
+    const inkeepDiv = document.createElement('div');
+    inkeepDiv.classList.add('inkeep-container');
+    inkeepDiv.id = 'inkeep';
+    document.body.appendChild(inkeepDiv);
 
-  const exampleBlock = createComponent<
-     IntegrationBlockProps,
-     IntegrationBlockState,
-     IntegrationAction,
-     IntegrationContext
-  >({
-    componentId: "inkeep-widget",
-    initialState: (props) => {
-      return {
-        message: "Click Me",
-      };
+    Inkeep().embed({
+        componentType: 'EmbeddedChat',
+        targetElement: document.getElementById('inkeep'),
+        properties: {
+            baseSettings: {
+                apiKey: 'YOUR_API_KEY', // required
+                integrationId: 'YOUR_INTEGRATION_ID', // required
+                organizationId: 'YOUR_ORGANIZATION_ID', // required
+                primaryBrandColor: '#26D6FF',
+                organizationDisplayName: 'Inkeep',
+                // ...optional settings
+                colorMode: {
+                    forcedColorMode: 'dark', // options: 'light' or dark'
+                },
+                theme: {
+                    components: {
+                        AIChatPageWrapper: {
+                            defaultProps: {
+                                size: {
+                                    base: 'expand',
+                                    md: 'expand',
+                                },
+                                variant: 'no-shadow', // 'no-shadow' or 'container-with-shadow'
+                            },
+                        },
+                    },
+                },
+            },
+            aiChatSettings: {
+                // ...optional settings
+                quickQuestions: [
+                    'Example question 1?',
+                    'Example question 2?',
+                    'Example question 3?',
+                ],
+                getHelpCallToActions: [
+                    {
+                        icon: { builtIn: 'FaSlack' },
+                        name: 'Slack',
+                        url: 'https://myorg.slack.com/C010101010',
+                    },
+                    {
+                        icon: { builtIn: 'FaDiscord' },
+                        name: 'Discord',
+                        url: 'https://discord.com/invite/invidecode123',
+                    },
+                    {
+                        icon: { builtIn: 'FaGithub' },
+                        name: 'GitHub',
+                        url: 'https://github.com/myorg/myrepo/discussions',
+                    },
+                ],
+            },
+            searchSettings: {
+                // optional settings
+            },
+            modalSettings: {
+                // optional settings
+            },
+        },
+    });
+};
+
+const handleFetchEvent: FetchEventCallback<IntegrationContext> = async () => {
+    return new Response(
+        `
+<html>
+  <style>
+    html, body {
+      margin: 0;
+      padding: 4px;
+    }
+  </style>
+  <body>
+    <script
+      defer
+      type="module"
+      src="https://unpkg.com/@inkeep/widgets-embed@${WIDGET_VERSION}/dist/embed.js"
+    ></script>
+    <script type="module" defer>
+      (${addInkeepWidget.toString()})()
+    </script>
+  </body>
+</html>
+`,
+        {
+            headers: {
+                'Content-Type': 'text/html',
+            },
+        }
+    );
+};
+
+const inkeepChatComponent = createComponent<
+    IntegrationBlockProps,
+    IntegrationBlockState,
+    IntegrationAction,
+    IntegrationContext
+>({
+    componentId: 'inkeep-widget',
+    initialState: () => {
+        return {
+            content: ``,
+        };
     },
-    action: async (element, action, context) => {
-      switch (action.action) {
-        case "click":
-          console.log("Button Clicked");
-          return {};
-      }
+    action: async () => {
+        return {};
     },
     render: async (element, context) => {
-      return (
-        <block>
-          <button label={element.state.message} onPress={{ action: "click" }} />
-        </block>
-      );
-    },
-  });
+        const { environment } = context;
 
-  export default createIntegration({
+        return (
+            <block>
+                <webframe
+                    source={{
+                        url: environment.integration.urls.publicEndpoint,
+                    }}
+                    aspectRatio={0.714286 / 1}
+                    data={{
+                        content: element.dynamicState('content'),
+                    }}
+                />
+            </block>
+        );
+    },
+});
+
+export default createIntegration({
     fetch: handleFetchEvent,
-    components: [exampleBlock],
-    events: {},
-  });
+    components: [inkeepChatComponent],
+});
