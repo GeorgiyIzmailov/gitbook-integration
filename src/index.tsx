8d6765bc78dae3c1ab94001ae658db7dc080187d
GeorgiyIzmailov <georgiy-izmailov-v@inkeep.com>
2024-02-07 14:15:38 +0300

    add InkeepConfig

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import {
     RuntimeContext,
 } from '@gitbook/runtime';
 
+import { addInkeepWidget } from '../InkeepConfig';
+
 type IntegrationContext = {} & RuntimeContext;
 type IntegrationBlockProps = { content?: string };
 type IntegrationBlockState = { content: string };
@@ -12,74 +14,6 @@ type IntegrationAction = {};
 
 const WIDGET_VERSION = '0.2.258';
 
-const addInkeepWidget = () => {
-    const inkeepDiv = document.createElement('div');
-    inkeepDiv.classList.add('inkeep-container');
-    inkeepDiv.id = 'inkeep';
-    document.body.appendChild(inkeepDiv);
-
-    Inkeep().embed({
-        componentType: 'EmbeddedChat',
-        targetElement: document.getElementById('inkeep'),
-        properties: {
-            baseSettings: {
-                apiKey: 'YOUR_API_KEY', // required
-                integrationId: 'YOUR_INTEGRATION_ID', // required
-                organizationId: 'YOUR_ORGANIZATION_ID', // required
-                primaryBrandColor: '#26D6FF',
-                organizationDisplayName: 'Inkeep',
-                // ...optional settings
-                colorMode: {
-                    forcedColorMode: 'dark', // options: 'light' or dark'
-                },
-                theme: {
-                    components: {
-                        AIChatPageWrapper: {
-                            defaultProps: {
-                                size: {
-                                    base: 'expand',
-                                    md: 'expand',
-                                },
-                                variant: 'no-shadow', // 'no-shadow' or 'container-with-shadow'
-                            },
-                        },
-                    },
-                },
-            },
-            aiChatSettings: {
-                // ...optional settings
-                quickQuestions: [
-                    'Example question 1?',
-                    'Example question 2?',
-                    'Example question 3?',
-                ],
-                getHelpCallToActions: [
-                    {
-                        icon: { builtIn: 'FaSlack' },
-                        name: 'Slack',
-                        url: 'https://myorg.slack.com/C010101010',
-                    },
-                    {
-                        icon: { builtIn: 'FaDiscord' },
-                        name: 'Discord',
-                        url: 'https://discord.com/invite/invidecode123',
-                    },
-                    {
-                        icon: { builtIn: 'FaGithub' },
-                        name: 'GitHub',
-                        url: 'https://github.com/myorg/myrepo/discussions',
-                    },
-                ],
-            },
-            searchSettings: {
-                // optional settings
-            },
-            modalSettings: {
-                // optional settings
-            },
-        },
-    });
-};
 
 const handleFetchEvent: FetchEventCallback<IntegrationContext> = async () => {
     return new Response(
